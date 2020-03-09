524a6badd9c348982ac086f007180126e24180a0
Kati <katiuscia.novaesdesa@georgebrown.ca>
2020-03-09 12:37:40 -0400

    Fixing path

--- server.js
+++ server.js
@@ -24,9 +24,19 @@ let routes = require("./routes/routes.js");
 
 app.use('/api',routes); 
 
-app.get('*',(req,res) =>{
-  res.sendFile(path.resolve(__dirname + '/client/build/index.html'))
-})
+// app.get('*',(req,res) =>{
+//   res.sendFile(path.resolve(__dirname + '/client/build/index.html'))
+// })
+
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static(path.join(__dirname, '/client/build')));
+
+  // Express serve up index.html file if it doesn't recognize route
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+  });
+}
 
 
 
